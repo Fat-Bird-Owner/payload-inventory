1aa50a0866d88b4e059e8872d2ef52935e67c9fa
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-19 15:06:11 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,54 @@
-var init = false;
-var inventory = null;
-var inventoryPane;
+var inventoryDialog = null;
+var inventoryPane = null;
 
-var inventoryblocks = [];
+Events.on(ClientLoadEvent, () => {
 
-Events.on(clientLoadEvent, () => {
 const statustable = Vars.ui.hudGroup.find("statustable");
 statustable.row();
-  
-const inventory = new Table();
-statustable.add(inventory).bottom().left();
 
-inventory.table(Tex.button, t => {
+const inventoryButton = new Table();
+statustable.add(inventoryButton).bottom().left();
+
 const button = new Button();
 button.image(Core.atlas.find("beryllium-wall-large")).size(40);
-
-t.add(button).row();
+inventoryButton.add(button);
 
 button.clicked(() => {
 
-if (inventory == null){
-inventory = new BaseDialog("inventory");
-inventory.addCloseButton();
+if(inventoryDialog == null){
+inventoryDialog = new BaseDialog("inventory");
+inventoryDialog.addCloseButton();
 
-inventory.cont.pane(p => {
-inventoryPane = new Table();
-inventoryPane.background(Tex.button);
-p.add(inventoryPane).grow();
+inventoryDialog.cont.pane(p => {
+
+inventoryPane = p;
+p.background(Tex.button);
+
+}).grow();
+}
+
+refreshPane();
+inventoryDialog.show();
+
 });
 
 function refreshPane(){
+if(!inventoryPane) return;
+
 inventoryPane.clear();
-for (let i = 0; i < blocks.length; i++){
-const block = blocks[i];
-const newButton = new Button();
-newButton.image(block.uiIcon);
 
-inventoryPane.add(newButton).grow().padTop(10);
-inventoryPane.add().width(10);
-}}
+for(let i = 0; i < Blocks.all.size; i++){
+const block = Blocks.all.get(i);
 
-refreshPane();
-inventory.show();
-  
-}});
-  
+const b = new Button();
+b.image(block.uiIcon).size(40);
+
+b.clicked(() => {
+print("Selected: " + block.name);
 });
 
-inventory.visibility = () => {
-return (Vars.ui.hudfrag.shown && !Vars.net.client() ? true : false)
+inventoryPane.add(b);
 }
-  
+}
+
 });

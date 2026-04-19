58e3d68cbfda6fdd10dc73e8e49cb8f4febaf2f2
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-19 15:02:22 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,21 @@
-const init = false;
-const inventory = null;
+var init = false;
+var inventory = null;
+var inventoryPane;
+
+var inventoryblocks = [];
 
 Events.on(clientLoadEvent, () => {
 const statustable = Vars.ui.hudGroup.find("statustable");
 statustable.row();
-
+  
 const inventory = new Table();
 statustable.add(inventory).bottom().left();
 
-tab.table(Tex.button, t => {
+inventory.table(Tex.button, t => {
 const button = new Button();
 button.image(Core.atlas.find("beryllium-wall-large")).size(40);
 
-t.add(button);
+t.add(button).row();
 
 button.clicked(() => {
 
@@ -20,11 +23,27 @@ if (inventory == null){
 inventory = new BaseDialog("inventory");
 inventory.addCloseButton();
 
-  
-
-}
-  
+inventory.cont.pane(p => {
+inventoryPane = new Table();
+inventoryPane.background(Tex.button);
+p.add(inventoryPane).grow();
 });
+
+function refreshPane(){
+inventoryPane.clear();
+for (let i = 0; i < blocks.length; i++){
+const block = blocks[i];
+const newButton = new Button();
+newButton.image(block.uiIcon);
+
+inventoryPane.add(newButton).grow().padTop(10);
+inventoryPane.add().width(10);
+}}
+
+refreshPane();
+inventory.show();
+  
+}});
   
 });
 
